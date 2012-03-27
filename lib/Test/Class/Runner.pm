d40c7d5f251ef341c70f22c5398e46f53367780b
Rob Hoelz <rob@hoelz.ro>
2012-03-27 16:30:25 +0200

    initial commit

---
+++ lib/Test/Class/Runner.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Test::Class::Runner;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT:  A short description of Test::Class::Runner
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
